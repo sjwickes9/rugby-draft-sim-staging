b8358430c86bbf209e8623fbd9c3e418716a3acd
sjwickes9 <simon@cynapse.co.uk>
2026-08-20 16:07:42 +0100

    Add files via upload

--- mp/lobby.js
+++ mp/lobby.js
@@ -5,7 +5,7 @@
 
 (function () {
     // Bumped on every change. Format v1.YYMMDDHHMM in GMT.
-    const VERSION = "v1.2608181610";
+    const VERSION = "v1.2608201504";
 
     const $ = function (id) { return document.getElementById(id); };
 
